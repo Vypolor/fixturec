3eca67f098c1ee3dbad4b0d74b34833a3a7c060a
Nikita Frolov <nfrolovm@ozon.ru>
2025-10-08 12:32:11 +0300

    [FIXTUREC-3] change file mode constants

--- internal/utils/file.go
+++ internal/utils/file.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"os"
-	"syscall"
 )
 
-const FilePerm0644 = os.FileMode(syscall.S_IRUSR | syscall.S_IWUSR | syscall.S_IRGRP | syscall.S_IROTH)
+const FilePerm0644 = os.FileMode(0644)
